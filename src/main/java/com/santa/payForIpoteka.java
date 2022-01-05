1081e66eb2236421c6b5d2a3d805b99336d9ed9b
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2022-01-06 01:07:15 +0400

    added firstPayOfIpoteka

---
+++ src/main/java/com/santa/payForIpoteka.java
@@ -0,0 +1,10 @@
+package com.santa;
+
+public class payForIpoteka {
+    public static void main(String[] args) {
+        int costOfHata = 3000000;
+        double firstPayForIpoteka = 0.2;
+        int sumOfFirstPay = (int) (costOfHata*firstPayForIpoteka);
+        System.out.println("sumOfFirstPay = " + sumOfFirstPay);
+    }
+}
