9c59ba619c831edb0a1c27faee5ac8fb7d00d3f2
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2022-01-06 01:25:01 +0400

    added ageOfUniversAndPeople

---
+++ src/main/java/com/santa/ageOfUniversAndPeople.java
@@ -0,0 +1,10 @@
+package com.santa;
+
+public class ageOfUniversAndPeople {
+    public static void main(String[] args) {
+        long ageOfUnivers = 13000000000L;
+        int ageOfPeople = 100000;
+        long howManyUniversOlderPeople = ageOfUnivers/ageOfPeople;
+        System.out.println("howManyUniversOlderPeople = " + howManyUniversOlderPeople);
+    }
+}
