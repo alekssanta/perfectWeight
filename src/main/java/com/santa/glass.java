1885ebc8834f0ed51e88d1bb811ecd2c0b54efb5
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2022-01-06 01:16:59 +0400

    added glass

---
+++ src/main/java/com/santa/glass.java
@@ -0,0 +1,11 @@
+package com.santa;
+
+public class glass {
+    public static void main(String[] args) {
+        int volumeOfGlass = 300;
+        double zapolnenieOfGlass = 0.6;
+        int volumeOfWaterOfGlass = (int) (volumeOfGlass*zapolnenieOfGlass);
+        System.out.println("volumeOfWaterOfGlass = " + volumeOfWaterOfGlass);
+
+    }
+}
