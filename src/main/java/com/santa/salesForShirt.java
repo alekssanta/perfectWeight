3a3cf66c4927bcc7fb73de57935751ceda8769d8
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2022-01-06 16:37:57 +0400

    added travelTime

---
+++ src/main/java/com/santa/salesForShirt.java
@@ -0,0 +1,20 @@
+package com.santa;
+
+public class salesForShirt {
+    public static void main(String[] args) {
+        int costOfShirt = 3000;
+        int countOfShirt = 5;
+        int countOfSalesForShirt = 3;
+        int countForSales = Math.abs(countOfShirt -countOfSalesForShirt*2);
+        System.out.println("countForSales = " + countForSales);
+        int costOfSelectedShirt = costOfShirt*countOfShirt;
+        int costOfAddShirt = countForSales*costOfShirt;
+        double procentForSales = 0.7;
+        double costOfSalesShirt = (countOfShirt+countForSales)*costOfShirt*procentForSales;
+        double procentOfSkidka = costOfSalesShirt/(costOfShirt*(countForSales+countForSales));
+        double saveMany = countOfSalesForShirt+costOfShirt*countForSales-costOfSalesShirt;
+        System.out.println("saveMany = " + saveMany);
+
+        System.out.println("procentOfSkidka = " + procentOfSkidka);
+    }
+}
