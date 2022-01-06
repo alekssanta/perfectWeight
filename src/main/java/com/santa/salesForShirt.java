b6e7f8e3264c4c4da4d2a329213bfc7a93c8f9d1
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2022-01-06 16:51:28 +0400

    added salesForShirt.java

--- src/main/java/com/santa/salesForShirt.java
+++ src/main/java/com/santa/salesForShirt.java
@@ -5,16 +5,16 @@ public class salesForShirt {
         int costOfShirt = 3000;
         int countOfShirt = 5;
         int countOfSalesForShirt = 3;
-        int countForSales = Math.abs(countOfShirt -countOfSalesForShirt*2);
+        int countForSales = Math.abs(countOfShirt - countOfSalesForShirt * 2);
         System.out.println("countForSales = " + countForSales);
-        int costOfSelectedShirt = costOfShirt*countOfShirt;
-        int costOfAddShirt = countForSales*costOfShirt;
+        int costOfSelectedShirt = costOfShirt * countOfShirt;
+        int costOfAddShirt = countForSales * costOfShirt;
         double procentForSales = 0.7;
-        double costOfSalesShirt = (countOfShirt+countForSales)*costOfShirt*procentForSales;
-        double procentOfSkidka = costOfSalesShirt/(costOfShirt*(countForSales+countForSales));
-        double saveMany = countOfSalesForShirt+costOfShirt*countForSales-costOfSalesShirt;
-        System.out.println("saveMany = " + saveMany);
-
-        System.out.println("procentOfSkidka = " + procentOfSkidka);
+        double costOfSalesShirt = (countOfShirt + countForSales) * costOfShirt * procentForSales;
+        double costOfSkidkaShirt = costOfSalesShirt / (costOfShirt * (countForSales + countOfShirt));
+        double saveMany = (countOfShirt + countForSales) * costOfShirt - costOfSalesShirt;
+        System.out.println("сэкономили = " + saveMany);
+        double freeShirt = saveMany/costOfShirt;
+        System.out.println("freeShirt = " + freeShirt);
     }
 }
